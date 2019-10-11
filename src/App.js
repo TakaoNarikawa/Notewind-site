1ab8e77a3c8f0b25eb81ff45d7bebc0ea7c6e1ca
narikawa <caprolactam2450@gmail.com>
2019-10-12 01:20:53 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import YouTube from 'react-youtube';
+
+// icon
+import logo from './icons/logo.png';
+import combinationIcon from './icons/combination.svg'
+import unsplitedIcon from './icons/unsplited.svg'
+import codeIcon from './icons/code.svg'
+
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Button from '@material-ui/core/Button';
 
 function App() {
+
+  const url = "https://apps.apple.com/us/app/notewind/id1482532727";
+
+  const AppWrapper = styled.div`
+  text-align: center;
+  `
+  const HeaderWrapper = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  `
+
+  const BodyWrapper = styled.div`
+  padding: 10vmin;
+  `
+
+  const FooterWrapper = styled.div`
+  background-color: #282c34;
+  min-height: 10vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: white;
+  `
+
+  const Logo = styled.img`
+    height: 40vmin;
+  `
+
+  const FeatureIcon = styled.img`
+    height: 15vmin;
+    padding: 4vmin;
+  `
+
+  const H1 = styled.h1`
+    padding-top: 4vmin;
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <AppWrapper>
+      <HeaderWrapper>
+        <Logo src={logo} className="App-logo" alt="logo" />
+        <h1>Notewind</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Any websites turn to your notebook.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        <Button
+          variant="contained"
+          color="secondary"
+          href={url}
+          startIcon={<ArrowDownward />}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Download
+      </Button>
+
+      </HeaderWrapper>
+      <BodyWrapper>
+        <YouTube
+          videoId="2g811Eo7K8U"
+        // opts={}
+        // onReady={}
+        />
+        <H1>
+          Combination of Note and Browser
+        </H1>
+        <FeatureIcon src={combinationIcon} />
+
+        <p>
+          This is not only note app, but also web app.
+        </p>
+        <p>
+          You can move to other site. Then you can begin new note.
+        </p>
+
+        <H1>
+          Unsplit View
+        </H1>
+        <FeatureIcon src={unsplitedIcon} />
+        <p>
+          You don't need to convert to PDF anymore.
+        </p>
+        <p>
+          You can write on continuous page.
+        </p>
+
+        <H1>
+          Native Functions
+        </H1>
+        <FeatureIcon src={codeIcon} />
+        <p>
+          Every web browser function
+        </p>
+        <p>
+          such as GIF image and javascript works correctly.
+        </p>
+      </BodyWrapper>
+
+      <FooterWrapper>
+        Copyright © Caprolactam2450 all rights reserved.
+      </FooterWrapper>
+    </AppWrapper>
   );
 }
 
